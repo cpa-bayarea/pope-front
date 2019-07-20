3075336f3657136c90ce7cfb7f228852c4b9cf91
Thiago Lago <thiagolagoo2@gmail.com>
2019-07-20 00:47:44 -0300

    :sparkles: [ADD] First Component - Search

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return <h1>POPE Página Inicial</h1>;
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaSearch />
+        Portal de Pesquisa - Justiça Comunitária <br />
+        <small>TJDFT</small>
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Pesquisar" />
+
+        <SubmitButton>
+          <FaSearch color="#000" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
-
-export default Main;
