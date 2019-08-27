b97b794ca9834ff0bc3423a51194a1b191d2a2ec
Thiago Lago <thiagolagoo2@gmail.com>
2019-08-26 22:17:15 -0300

    :art: prepare for apresentation

--- frontend/src/pages/Results/index.js
+++ frontend/src/pages/Results/index.js
@@ -7,11 +7,11 @@ import {
   FaList,
   FaThumbsUp,
 } from 'react-icons/fa';
-// import api from '../../services/api';
 import Card from 'react-bootstrap/Card';
 import { Container, Form, SubmitButton, Logo, Panel, Title } from './styles';
 import Footer from '../../components/Footer/Footer';
 import logoTjdft from '../../assets/img/tjdft.png';
+import ModalComponent from '../../components/Modal/Modal';
 
 export default class Results extends Component {
   // constructor(props) {
@@ -52,10 +52,19 @@ export default class Results extends Component {
   // }
 
   state = {
-    title: 'Cartório da Ceilândia',
-    adress: 'QNM Alguma Coisa 04',
-    tell: '(61) 3333-0000',
-    attendence: '08h às 19h',
+    title: 'Ofício de Notas e Protesto de Títulos de Ceilândia',
+    adress: 'CNM 01, BLOCO H, LOJAS 01/03',
+    tell: '(61) 3371-9000',
+    attendence: '09h às 18h',
+    services: 'Autenticação de Documentos',
+  };
+
+  state2 = {
+    title:
+      'Ofício de Registro Civil, Títulos e Documentos e Pessoas Júridicas de Ceilândia',
+    adress: 'QS 614 LT 1/2 CJ C',
+    tell: '(61) 3458-4809',
+    attendence: '09h às 18h',
     services: 'Autenticação de Documentos',
   };
 
@@ -68,6 +77,7 @@ export default class Results extends Component {
   render() {
     return (
       <>
+        <ModalComponent />
         <Logo>
           <img
             width="100"
@@ -101,30 +111,218 @@ export default class Results extends Component {
         <Panel>
           <div>
             <div>
-              <Card style={{ width: '18rem' }} className="ml-2 mr-2">
-                <Card.Body>
-                  <Title> {this.state.title} </Title>
-                  <br />
-                  <tr>
-                    <FaMapMarkerAlt />
-                    {this.state.adress}
-                  </tr>
-                  <tr>
-                    <FaPhone />
-                    {this.state.tell}
-                  </tr>
-                  <tr>
-                    <FaClock />
-                    {this.state.attendence}
-                  </tr>
-                  <tr>
-                    <FaList />
-                    {this.state.services}
-                  </tr>
-                  <FaThumbsUp />
-                  Possui Serviços Gratuitos
-                </Card.Body>
-              </Card>
+              <td>
+                <Card
+                  style={{ width: '21rem', marginBottom: 10 }}
+                  className="ml-2 mr-2"
+                >
+                  <Card.Body>
+                    <Title> {this.state.title} </Title>
+                    <br />
+                    <tr>
+                      <FaMapMarkerAlt />
+                      {this.state.adress}
+                    </tr>
+                    <tr>
+                      <FaPhone />
+                      {this.state.tell}
+                    </tr>
+                    <tr>
+                      <FaClock />
+                      {this.state.attendence}
+                    </tr>
+                    <tr>
+                      <FaList />
+                      {this.state.services}
+                    </tr>
+                    <FaThumbsUp />
+                    Possui Serviços Gratuitos
+                  </Card.Body>
+                </Card>
+                <Card style={{ width: '21rem' }} className="ml-2 mr-2">
+                  <Card.Body>
+                    <Title> {this.state2.title} </Title>
+                    <br />
+                    <tr>
+                      <FaMapMarkerAlt />
+                      {this.state2.adress}
+                    </tr>
+                    <tr>
+                      <FaPhone />
+                      {this.state2.tell}
+                    </tr>
+                    <tr>
+                      <FaClock />
+                      {this.state2.attendence}
+                    </tr>
+                    <tr>
+                      <FaList />
+                      {this.state2.services}
+                    </tr>
+                    <FaThumbsUp />
+                    Possui Serviços Gratuitos
+                  </Card.Body>
+                </Card>
+              </td>
+              <td>
+                <Card
+                  style={{ width: '21rem', marginBottom: 10 }}
+                  className="ml-2 mr-2"
+                >
+                  <Card.Body>
+                    <Title> {this.state.title} </Title>
+                    <br />
+                    <tr>
+                      <FaMapMarkerAlt />
+                      {this.state.adress}
+                    </tr>
+                    <tr>
+                      <FaPhone />
+                      {this.state.tell}
+                    </tr>
+                    <tr>
+                      <FaClock />
+                      {this.state.attendence}
+                    </tr>
+                    <tr>
+                      <FaList />
+                      {this.state.services}
+                    </tr>
+                    <FaThumbsUp />
+                    Possui Serviços Gratuitos
+                  </Card.Body>
+                </Card>
+                <Card style={{ width: '21rem' }} className="ml-2 mr-2">
+                  <Card.Body>
+                    <Title> {this.state2.title} </Title>
+                    <br />
+                    <tr>
+                      <FaMapMarkerAlt />
+                      {this.state2.adress}
+                    </tr>
+                    <tr>
+                      <FaPhone />
+                      {this.state2.tell}
+                    </tr>
+                    <tr>
+                      <FaClock />
+                      {this.state2.attendence}
+                    </tr>
+                    <tr>
+                      <FaList />
+                      {this.state2.services}
+                    </tr>
+                    <FaThumbsUp />
+                    Possui Serviços Gratuitos
+                  </Card.Body>
+                </Card>
+              </td>
+              <td>
+                <Card
+                  style={{ width: '21rem', marginBottom: 10 }}
+                  className="ml-2 mr-2"
+                >
+                  <Card.Body>
+                    <Title> {this.state.title} </Title>
+                    <br />
+                    <tr>
+                      <FaMapMarkerAlt />
+                      {this.state.adress}
+                    </tr>
+                    <tr>
+                      <FaPhone />
+                      {this.state.tell}
+                    </tr>
+                    <tr>
+                      <FaClock />
+                      {this.state.attendence}
+                    </tr>
+                    <tr>
+                      <FaList />
+                      {this.state.services}
+                    </tr>
+                    <FaThumbsUp />
+                    Possui Serviços Gratuitos
+                  </Card.Body>
+                </Card>
+                <Card style={{ width: '21rem' }} className="ml-2 mr-2">
+                  <Card.Body>
+                    <Title> {this.state2.title} </Title>
+                    <br />
+                    <tr>
+                      <FaMapMarkerAlt />
+                      {this.state2.adress}
+                    </tr>
+                    <tr>
+                      <FaPhone />
+                      {this.state2.tell}
+                    </tr>
+                    <tr>
+                      <FaClock />
+                      {this.state2.attendence}
+                    </tr>
+                    <tr>
+                      <FaList />
+                      {this.state2.services}
+                    </tr>
+                    <FaThumbsUp />
+                    Possui Serviços Gratuitos
+                  </Card.Body>
+                </Card>
+              </td>
+              <td>
+                <Card
+                  style={{ width: '21rem', marginBottom: 10 }}
+                  className="ml-2 mr-2"
+                >
+                  <Card.Body>
+                    <Title> {this.state.title} </Title>
+                    <br />
+                    <tr>
+                      <FaMapMarkerAlt />
+                      {this.state.adress}
+                    </tr>
+                    <tr>
+                      <FaPhone />
+                      {this.state.tell}
+                    </tr>
+                    <tr>
+                      <FaClock />
+                      {this.state.attendence}
+                    </tr>
+                    <tr>
+                      <FaList />
+                      {this.state.services}
+                    </tr>
+                    <FaThumbsUp />
+                    Possui Serviços Gratuitos
+                  </Card.Body>
+                </Card>
+                <Card style={{ width: '21rem' }} className="ml-2 mr-2">
+                  <Card.Body>
+                    <Title> {this.state2.title} </Title>
+                    <br />
+                    <tr>
+                      <FaMapMarkerAlt />
+                      {this.state2.adress}
+                    </tr>
+                    <tr>
+                      <FaPhone />
+                      {this.state2.tell}
+                    </tr>
+                    <tr>
+                      <FaClock />
+                      {this.state2.attendence}
+                    </tr>
+                    <tr>
+                      <FaList />
+                      {this.state2.services}
+                    </tr>
+                    <FaThumbsUp />
+                    Possui Serviços Gratuitos
+                  </Card.Body>
+                </Card>
+              </td>
             </div>
           </div>
         </Panel>
